5d7efd6eec73ccfeb2cf1a02c1f077f17d33ba25
Martin Smith <masterpop@pop-os.localdomain>
2021-01-29 19:53:29 +0200

    Stated the backend for game-slots

--- public/javascripts/games/slots/main.js
+++ public/javascripts/games/slots/main.js
@@ -1,97 +1,114 @@
-document.getElementById("Gira").addEventListener("click", doSlot);
+document.getElementById('Gira').addEventListener('click', doSlot);
 
 var doing = false;
-var spin = [new Audio("res/sounds/spin.mp3"),new Audio("res/sounds/spin.mp3"),new Audio("res/sounds/spin.mp3"),new Audio("res/sounds/spin.mp3"),new Audio("res/sounds/spin.mp3"),new Audio("res/sounds/spin.mp3"),new Audio("res/sounds/spin.mp3")];
-var coin = [new Audio("res/sounds/coin.mp3"),new Audio("res/sounds/coin.mp3"),new Audio("res/sounds/coin.mp3")]
-var win = new Audio("res/sounds/win.mp3");
-var lose = new Audio("res/sounds/lose.mp3");
+var spin = [
+  new Audio('res/sounds/spin.mp3'),
+  new Audio('res/sounds/spin.mp3'),
+  new Audio('res/sounds/spin.mp3'),
+  new Audio('res/sounds/spin.mp3'),
+  new Audio('res/sounds/spin.mp3'),
+  new Audio('res/sounds/spin.mp3'),
+  new Audio('res/sounds/spin.mp3'),
+];
+var coin = [
+  new Audio('res/sounds/coin.mp3'),
+  new Audio('res/sounds/coin.mp3'),
+  new Audio('res/sounds/coin.mp3'),
+];
+var win = new Audio('res/sounds/win.mp3');
+var lose = new Audio('res/sounds/lose.mp3');
 var audio = false;
-let status = document.getElementById("status")
+let status = document.getElementById('status');
 var info = true;
 
-function doSlot(){
-	if (doing){return null;}
-	doing = true;
-	var numChanges = randomInt(1,4)*7
-	var numberSlot1 = numChanges+randomInt(1,7)
-	var numberSlot2 = numChanges+2*7+randomInt(1,7)
-	var numberSlot3 = numChanges+4*7+randomInt(1,7)
+function doSlot() {
+  if (doing) {
+    return null;
+  }
+  doing = true;
+  var numChanges = randomInt(1, 4) * 7;
+  var numberSlot1 = numChanges + randomInt(1, 7);
+  var numberSlot2 = numChanges + 2 * 7 + randomInt(1, 7);
+  var numberSlot3 = numChanges + 4 * 7 + randomInt(1, 7);
 
-	var i1 = 0;
-	var i2 = 0;
-	var i3 = 0;
-	var sound = 0
-	status.innerHTML = "SPINNING"
-	slot1 = setInterval(spin1, 50);
-	slot2 = setInterval(spin2, 50);
-	slot3 = setInterval(spin3, 50);
-	function spin1(){
-		i1++;
-		if (i1>=numberSlot1){
-			//coin[0].play()
-			clearInterval(slot1);
-			return null;
-		}
-		slotTile = document.getElementById("slot1");
-		if (slotTile.className=="a7"){
-			slotTile.className = "a0";
-		}
-		slotTile.className = "a"+(parseInt(slotTile.className.substring(1))+1)
-	}
-	function spin2(){
-		i2++;
-		if (i2>=numberSlot2){
-			//coin[1].play()
-			clearInterval(slot2);
-			return null;
-		}
-		slotTile = document.getElementById("slot2");
-		if (slotTile.className=="a7"){
-			slotTile.className = "a0";
-		}
-		slotTile.className = "a"+(parseInt(slotTile.className.substring(1))+1)
-	}
-	function spin3(){
-		i3++;
-		if (i3>=numberSlot3){
-			//coin[2].play()
-			clearInterval(slot3);
-			testWin();
-			return null;
-		}
-		slotTile = document.getElementById("slot3");
-		if (slotTile.className=="a7"){
-			slotTile.className = "a0";
-		}
-		sound++;
-		if (sound==spin.length){
-			sound=0;
-		}
-		//spin[sound].play();
-		slotTile.className = "a"+(parseInt(slotTile.className.substring(1))+1)
-	}
+  var i1 = 0;
+  var i2 = 0;
+  var i3 = 0;
+  var sound = 0;
+  status.innerHTML = 'SPINNING';
+  slot1 = setInterval(spin1, 50);
+  slot2 = setInterval(spin2, 50);
+  slot3 = setInterval(spin3, 50);
+  function spin1() {
+    i1++;
+    if (i1 >= numberSlot1) {
+      //coin[0].play()
+      clearInterval(slot1);
+      return null;
+    }
+    slotTile = document.getElementById('slot1');
+    if (slotTile.className == 'a7') {
+      slotTile.className = 'a0';
+    }
+    slotTile.className = 'a' + (parseInt(slotTile.className.substring(1)) + 1);
+  }
+  function spin2() {
+    i2++;
+    if (i2 >= numberSlot2) {
+      //coin[1].play()
+      clearInterval(slot2);
+      return null;
+    }
+    slotTile = document.getElementById('slot2');
+    if (slotTile.className == 'a7') {
+      slotTile.className = 'a0';
+    }
+    slotTile.className = 'a' + (parseInt(slotTile.className.substring(1)) + 1);
+  }
+  function spin3() {
+    i3++;
+    if (i3 >= numberSlot3) {
+      //coin[2].play()
+      clearInterval(slot3);
+      testWin();
+      return null;
+    }
+    slotTile = document.getElementById('slot3');
+    if (slotTile.className == 'a7') {
+      slotTile.className = 'a0';
+    }
+    sound++;
+    if (sound == spin.length) {
+      sound = 0;
+    }
+    //spin[sound].play();
+    slotTile.className = 'a' + (parseInt(slotTile.className.substring(1)) + 1);
+  }
 }
 
-function testWin(){
-	var slot1 = document.getElementById("slot1").className
-	var slot2 = document.getElementById("slot2").className
-	var slot3 = document.getElementById("slot3").className
+function testWin() {
+  var slot1 = document.getElementById('slot1').className;
+  var slot2 = document.getElementById('slot2').className;
+  var slot3 = document.getElementById('slot3').className;
 
-	if (((slot1 == slot2 && slot2 == slot3) ||
-		(slot1 == slot2 && slot3 == "a7") ||
-		(slot1 == slot3 && slot2 == "a7") ||
-		(slot2 == slot3 && slot1 == "a7") ||
-		(slot1 == slot2 && slot1 == "a7") ||
-		(slot1 == slot3 && slot1 == "a7") ||
-		(slot2 == slot3 && slot2 == "a7") ) && !(slot1 == slot2 && slot2 == slot3 && slot1=="a7")){
-		status.innerHTML = "YOU WIN!";
-		//win.play();
-	}else{
-		status.innerHTML = "YOU LOSE!"
-		//lose.play();
-	}
-	doing = false;
+  if (
+    ((slot1 == slot2 && slot2 == slot3) ||
+      (slot1 == slot2 && slot3 == 'a7') ||
+      (slot1 == slot3 && slot2 == 'a7') ||
+      (slot2 == slot3 && slot1 == 'a7') ||
+      (slot1 == slot2 && slot1 == 'a7') ||
+      (slot1 == slot3 && slot1 == 'a7') ||
+      (slot2 == slot3 && slot2 == 'a7')) &&
+    !(slot1 == slot2 && slot2 == slot3 && slot1 == 'a7')
+  ) {
+    status.innerHTML = 'YOU WIN!';
+    //win.play();
+  } else {
+    status.innerHTML = 'YOU LOSE!';
+    //lose.play();
+  }
+  doing = false;
 }
-function randomInt(min, max){
-	return Math.floor((Math.random() * (max-min+1)) + min);
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
--- routes/games/slots.js
+++ routes/games/slots.js
@@ -15,4 +15,47 @@ router.get('/', function (req, res, next) {
   }
 });
 
+router.post('/spin', (req, res, next) => {
+  if (!req.session.loggedIn) {
+    logger.warn('spun slots without logging in');
+    res.redirect(303, '/');
+    return;
+  }
+  let slot1 = randomSlotNum();
+  let slot2 = randomSlotNum();
+  let slot3 = randomSlotNum();
+
+  res.send({ slots: [slot1, slot2, slot3] });
+  // let userID = req.session.userID;
+  let gameID = 1; //Slots' game id
+  let winnings = req.body.amount;
+
+  dbPool.query(
+    'INSERT INTO play_history (user_id, game_id, winnings, time) VALUES(?, ?, ?, CURRENT_TIME())',
+    [userID, gameID, winnings],
+    (err, rows) => {
+      if (err) {
+        logger.error(`DB error on /roulette (${req.ip}):`);
+        next(err);
+        return;
+      }
+    }
+  );
+  dbPool.query(
+    'UPDATE users SET balance = balance + ? WHERE id = ?',
+    [winnings, userID],
+    (err, rows) => {
+      if (err) {
+        logger.error(`DB error on /roulette (${req.ip}):`);
+        next(err);
+        return;
+      }
+    }
+  );
+});
+
+function randomSlotNum() {
+  return Math.floor(Math.random() * 7 + 1);
+}
+
 module.exports = router;
