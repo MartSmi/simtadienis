70f98dd0c63104a1ed33ccae5e76f11746137242
MartSmi <martynasmitas@gmail.com>
2021-02-21 09:32:54 +0200

    Added /start route and styled snake.js

--- routes/games/snake.js
+++ routes/games/snake.js
@@ -1,9 +1,9 @@
 const appRoot = require('app-root-path');
 const express = require('express');
-// var dbPool = require(appRoot + '/db').pool;
-// var mysql = require('mysql');
+var dbPool = require(appRoot + '/db').pool;
 var logger = require(appRoot + '/logger');
 var router = express.Router();
+const gameID = 2; //Snake game id
 const enterTimestamp = process.env.ENTER_TIMESTAMP;
 
 router.get('/', function (req, res, next) {
@@ -18,10 +18,31 @@ router.get('/', function (req, res, next) {
   } else {
     var opts = {
       // name: req.session.fullName,
-      balance: req.session.balance
-    }
+      balance: req.session.balance,
+    };
     res.render('games/snake', opts);
   }
+});
+
+router.post('/start', function (req, res, next) {
+  if (!req.session.loggedIn) {
+    logger.warn('attempt to bet in /blackjack without logging in');
+    res.redirect(303, '/');
+    return;
+  }
+  const userID = req.session.userID;
+  dbPool.query(
+    'INSERT INTO play_history (user_id, game_id, time) VALUES(?, ?, CURRENT_TIME())',
+    [userID, gameID],
+    (err, row) => {
+      if (err) {
+        logger.error(`DB error on /blackjack (${req.ip}):`);
+        next(err);
+        return;
+      }
+      res.send({ gameSessionID: row.insertId });
+    }
+  );
 });
 
 module.exports = router;
