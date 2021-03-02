0cfe1ceadba683b68d023693477ceadad2e28a23
MartSmi <martynasmitas@gmail.com>
2021-03-02 09:24:49 +0200

    Added basic tetris backend

--- routes/games/tetris.js
+++ routes/games/tetris.js
@@ -1,9 +1,12 @@
 const appRoot = require('app-root-path');
 const express = require('express');
+const winston = require('winston/lib/winston/config');
 // var dbPool = require(appRoot + '/db').pool;
-// var mysql = require('mysql');
-var logger = require(appRoot + '/logger');
-var router = express.Router();
+const logger = require(appRoot + '/logger');
+const router = express.Router();
+const balance = require(appRoot + '/services/balance');
+const playHistory = require(appRoot + '/services/playHistory');
+const gameID = 4; // Tetris gameID
 const enterTimestamp = process.env.ENTER_TIMESTAMP;
 
 router.get('/', function (req, res, next) {
@@ -18,10 +21,45 @@ router.get('/', function (req, res, next) {
   } else {
     var opts = {
       // name: req.session.fullName,
-      balance: req.session.balance
-    }
+      balance: req.session.balance,
+    };
     res.render('games/tetris', opts);
   }
 });
 
+router.get('/start', function (req, res, next) {
+  if (!req.session.loggedIn) {
+    logger.warn('attempt to start a game in /tetris without logging in');
+    res.redirect(303, '/');
+    return;
+  }
+  const userID = req.session.userID;
+  playHistory.insert(userID, gameID).then(row => {
+    res.send({ gameSessionID: row.insertId });
+  });
+});
+
+router.post('/end', function (req, res, next) {
+  if (!req.session.loggedIn) {
+    logger.warn('attempt to end a game in /tetris without logging in');
+    res.redirect(303, '/');
+    return;
+  }
+  const userID = req.body.userID;
+  const gameSessionID = req.body.gameSessionID;
+  const score = req.body.score;
+  const winnings = Math.round(score / 100);
+  playHistory.update(gameSessionID, winnings);
+  balance
+    .update(winnings, userID)
+    .catch(err => {
+      logger.error(`DB error on /tetris (balance) (${req.ip}):`);
+      next(err);
+      return;
+    })
+    .finally(() => {
+      req.session.balance += winnings;
+      res.sendStatus(200);
+    });
+});
 module.exports = router;
